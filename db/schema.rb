c66e7e2dd34df622641fc7a3e240809d64b6f5b7
Mark Sutherland <marksutherland@Marks-MacBook-Pro-6.local>
2024-02-09 16:50:06 -0600

    pushing meeting model to dev

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_05_025502) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_09_224751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_05_025502) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "meetings", force: :cascade do |t|
+    t.string "meeting_id"
+    t.string "meeting_name"
+    t.datetime "meeting_time"
+    t.integer "meeting_attendance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
