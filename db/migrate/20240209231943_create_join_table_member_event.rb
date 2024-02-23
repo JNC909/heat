00f3c1b80d3158d1493eec9889663457212046d1
kritikaba17 <kritikaba17@gmail.com>
2024-02-23 17:52:59 -0600

    Added a navigation bar along with other functionalities

--- db/migrate/20240209231943_create_join_table_member_event.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableMemberEvent < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :members, :events do |t|
-      # t.index [:member_id, :event_id]
-      # t.index [:event_id, :member_id]
-    end
-  end
-end
