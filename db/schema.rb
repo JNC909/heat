9ad9b7d8cfb3d8a2414c21cda21c4097fbf4b3d5
Mark Sutherland <marksutherland@Marks-MacBook-Pro-6.local>
2024-02-26 17:06:39 -0600

    added member entity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_26_203144) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_26_230434) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.date "date"
@@ -23,6 +23,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_26_203144) do
     t.string "name"
     t.date "date"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "member_name"
+    t.integer "member_points"
+    t.boolean "executive_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
