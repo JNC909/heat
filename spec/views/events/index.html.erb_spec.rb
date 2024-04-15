c0ed58ff803a318103b857a76fda9be02d261f34
kritikaba17 <kritikaba17@gmail.com>
2024-04-15 17:33:09 -0500

    Fixed the rspec files for events

--- spec/views/events/index.html.erb_spec.rb
+++ spec/views/events/index.html.erb_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "events/index", type: :view do
   before(:each) do
     assign(:events, [
-             Event.create!(event_name: "Event 1", event_link: "Link 1", event_datetime: DateTime.now),
-             Event.create!(event_name: "Event 2", event_link: "Link 2", event_datetime: DateTime.now)
-           ])
+      Event.create!(event_name: "Event 1", event_link: "Link 1", event_datetime: DateTime.now, event_end: DateTime.now),
+      Event.create!(event_name: "Event 2", event_link: "Link 2", event_datetime: DateTime.now, event_end: DateTime.now)
+    ])
   end
 
   it "renders a list of events" do
@@ -13,5 +13,6 @@ RSpec.describe "events/index", type: :view do
     expect(rendered).to match(/Event 1/)
     expect(rendered).to match(/Event 2/)
     expect(rendered).to have_selector("div.event", count: 2)
+
   end
 end
--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "events/show", type: :view do
+RSpec.describe "events/index", type: :view do
   before(:each) do
-    @event = assign(:event, Event.create!(event_name: "Event Title", event_link: "Link", event_datetime: DateTime.now))
+    assign(:events, [
+      Event.create!(event_name: "Event 1", event_link: "Link 1", event_datetime: DateTime.now, event_end: DateTime.now),
+      Event.create!(event_name: "Event 2", event_link: "Link 2", event_datetime: DateTime.now, event_end: DateTime.now)
+    ])
   end
 
-  it "renders event actions" do
+  it "renders a list of events" do
     render
-    expect(rendered).to match(/Edit this event/)
-    expect(rendered).to match(/Back to events/)
+    expect(rendered).to match(/Event 1/)
+    expect(rendered).to match(/Event 2/)
+    expect(rendered).to have_selector("div.event", count: 2)
+
   end
 end
